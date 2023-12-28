b7e184d1052c3ac10b21a58b5cafbc466e897988
karlosgr <karlosgr0201@gmail.com>
2023-12-28 17:13:29 -0500

    feat: starting with the user list of services or options

--- bisnes/lib/src/Pages/User/user_info_page.dart
+++ bisnes/lib/src/Pages/User/user_info_page.dart
@@ -2,8 +2,8 @@
 import 'package:flutter/material.dart';
 
 //Internal Imports
-import '../../Utils/custom_icons.dart';
 import '../../Utils/interfaces.dart';
+import 'Widgets/user_page_widgets.dart';
 
 class UserInfoPage extends StatelessWidget {
   const UserInfoPage({super.key});
@@ -17,89 +17,9 @@ class UserInfoPage extends StatelessWidget {
         body: Column(
           children: [
             profile(),
-            userInformation(),
           ],
         ),
       ),
     );
   }
-}
-
-PreferredSizeWidget notificationButtonBar() {
-  return AppBar(
-    toolbarHeight: 80,
-    elevation: 0,
-    backgroundColor: const Color.fromRGBO(245, 246, 248, 1),
-    foregroundColor: const Color.fromRGBO(114, 124, 142, 1),
-    actions: [
-      Container(
-          margin: const EdgeInsets.only(right: 30, top: 20),
-          child: Badge(
-            backgroundColor: const Color.fromRGBO(29, 176, 3, 1),
-            alignment: AlignmentDirectional.bottomStart,
-            label: const Text('45'),
-            child: IconButton(
-              onPressed: () {},
-              icon: const Icon(CustomIcons.notifications),
-              iconSize: 32,
-            ),
-          )),
-    ],
-  );
-}
-
-Widget profile() {
-  return Row(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      profilePhoto(),
-    ],
-  );
-}
-
-Widget profilePhoto() {
-  return Container(
-      margin: const EdgeInsets.only(left: 15),
-      child: ClipOval(
-        child: SizedBox.fromSize(
-          size: const Size.fromRadius(70),
-          child: const Image(
-            image: AssetImage("assets/Images/photo.png"),
-            fit: BoxFit.fill,
-          ),
-        ),
-      ));
-}
-
-Widget userInformation() {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.start,
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      const Padding(
-        padding: EdgeInsets.only(top: 20),
-        child: Text(
-          "Username",
-          style: TextStyle(fontSize: 24, color: fontAppColor),
-        ),
-      ),
-      const Padding(
-        padding: EdgeInsets.only(top: 5),
-        child: Text(
-          "testemail@gmail.com",
-          style: TextStyle(
-            color: fontAppColor,
-            fontSize: 16,
-          ),
-        ),
-      ),
-      ElevatedButton(
-          style: const ButtonStyle(),
-          onPressed: () => {},
-          child: const Text(
-            "Editar Usuario",
-            style: TextStyle(color: Color.fromRGBO(114, 124, 142, 1)),
-          ))
-    ],
-  );
 }
--- bisnes/lib/src/Utils/interfaces.dart
+++ bisnes/lib/src/Utils/interfaces.dart
@@ -3,3 +3,4 @@ import 'dart:ui';
 //Colors
 const backgroundAppColor = Color.fromRGBO(245, 246, 248, 1);
 const fontAppColor = Color.fromRGBO(81, 92, 111, 1);
+const iconAppColor = Color.fromRGBO(114, 124, 142, 1);
